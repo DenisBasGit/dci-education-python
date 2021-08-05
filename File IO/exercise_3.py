3c888bea7ff62ba3289989db8f0af8dcce2bbc89
Denis Bas <84315357+DenisBasGit@users.noreply.github.com>
2021-08-05 12:21:38 +0300

    Delete File IO directory

--- File IO/exercise_3.py
+++
@@ -1,9 +0,0 @@
-# Open the file in binary mode
-with open('exercise_3.txt', 'rb') as f:
-    # Read all from it
-    bin_text = f.read()
-
-# Open other file to write a normal text
-with open('exercise_3_out.txt', 'w') as f:
-    # To convert a bytes-like object, use decode()
-    f.write(bin_text.decode('utf-8'))
